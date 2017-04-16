4273d2f5423cab47391d8cd9af5a4922cebe794a
AbhinavKumar90 <abhinav.kanwar@gmail.com>
2017-04-15 20:30:57 -0700

    second commit

--- app/src/main/java/com/visonus/abhi/visonus/FirstActivity.java
+++ app/src/main/java/com/visonus/abhi/visonus/FirstActivity.java
@@ -1,13 +1,23 @@
 package com.visonus.abhi.visonus;
 
 import android.content.Intent;
+import android.net.ParseException;
 import android.net.Uri;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.EditText;
 import android.widget.Button;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class FirstActivity extends AppCompatActivity {
 
@@ -20,7 +30,7 @@ public class FirstActivity extends AppCompatActivity {
         //setContentView(R.layout.activity_first);
 
         openCameraListener();
-        uploadImage();
+        openGallery();
     }
 
     //Method to open camera
@@ -36,7 +46,7 @@ public class FirstActivity extends AppCompatActivity {
     }
 
     //Method to open gallery
-    public void uploadImage(){
+    public void openGallery(){
         Button gallery = (Button)findViewById(R.id.uploadImage);
         gallery.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -45,14 +55,125 @@ public class FirstActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+
     }
 
-    /** Called when the user clicks the Send button */
-    public void sendMessage(View view) {
-        Intent intent = new Intent(this, DisplayMessageActivity.class);
-        EditText editText = (EditText) findViewById(R.id.edit_message);
-        String message = editText.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
-        startActivity(intent);
+    //Sending image to the server using multi-part request
+    public String multipartRequest(String urlTo, String post, String filepath, String filefield) throws ParseException, IOException {
+
+        HttpURLConnection connection = null;
+        DataOutputStream outputStream = null;
+        InputStream inputStream = null;
+
+        String twoHyphens = "--";
+        String boundary =  "*****"+Long.toString(System.currentTimeMillis())+"*****";
+        String lineEnd = "\r\n";
+
+        String result = "";
+
+        int bytesRead, bytesAvailable, bufferSize;
+        byte[] buffer;
+        int maxBufferSize = 1*1024*1024;
+
+        String[] q = filepath.split("/");
+        int idx = q.length - 1;
+
+        try {
+            File file = new File(filepath);
+            FileInputStream fileInputStream = new FileInputStream(file);
+
+            URL url = new URL(urlTo);
+            connection = (HttpURLConnection) url.openConnection();
+
+            connection.setDoInput(true);
+            connection.setDoOutput(true);
+            connection.setUseCaches(false);
+
+            connection.setRequestMethod("POST");
+            connection.setRequestProperty("Connection", "Keep-Alive");
+            connection.setRequestProperty("User-Agent", "Android Multipart HTTP Client 1.0");
+            connection.setRequestProperty("Content-Type", "multipart/form-data; boundary="+boundary);
+
+            outputStream = new DataOutputStream(connection.getOutputStream());
+            outputStream.writeBytes(twoHyphens + boundary + lineEnd);
+            outputStream.writeBytes("Content-Disposition: form-data; name=\"" + filefield + "\"; filename=\"" + q[idx] +"\"" + lineEnd);
+            outputStream.writeBytes("Content-Type: image/jpeg" + lineEnd);
+            outputStream.writeBytes("Content-Transfer-Encoding: binary" + lineEnd);
+            outputStream.writeBytes(lineEnd);
+
+            bytesAvailable = fileInputStream.available();
+            bufferSize = Math.min(bytesAvailable, maxBufferSize);
+            buffer = new byte[bufferSize];
+
+            bytesRead = fileInputStream.read(buffer, 0, bufferSize);
+            while(bytesRead > 0) {
+                outputStream.write(buffer, 0, bufferSize);
+                bytesAvailable = fileInputStream.available();
+                bufferSize = Math.min(bytesAvailable, maxBufferSize);
+                bytesRead = fileInputStream.read(buffer, 0, bufferSize);
+            }
+
+            outputStream.writeBytes(lineEnd);
+
+            // Upload POST Data
+            String[] posts = post.split("&");
+            int max = posts.length;
+            for(int i=0; i<max;i++) {
+                outputStream.writeBytes(twoHyphens + boundary + lineEnd);
+                String[] kv = posts[i].split("=");
+                outputStream.writeBytes("Content-Disposition: form-data; name=\"" + kv[0] + "\"" + lineEnd);
+                outputStream.writeBytes("Content-Type: text/plain"+lineEnd);
+                outputStream.writeBytes(lineEnd);
+                outputStream.writeBytes(kv[1]);
+                outputStream.writeBytes(lineEnd);
+            }
+
+            outputStream.writeBytes(twoHyphens + boundary + twoHyphens + lineEnd);
+
+            inputStream = connection.getInputStream();
+            result = this.convertStreamToString(inputStream);
+
+            fileInputStream.close();
+            inputStream.close();
+            outputStream.flush();
+            outputStream.close();
+
+            return result;
+        } catch(Exception e) {
+            Log.e("MultipartRequest","Multipart Form Upload Error");
+            e.printStackTrace();
+            return "error";
+        }
+    }
+
+    private String convertStreamToString(InputStream is) {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+        StringBuilder sb = new StringBuilder();
+
+        String line = null;
+        try {
+            while ((line = reader.readLine()) != null) {
+                sb.append(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                is.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return sb.toString();
     }
 }
+
+    /** Called when the user clicks the Send button */
+//    public void sendMessage(View view) {
+//        Intent intent = new Intent(this, DisplayMessageActivity.class);
+//        EditText editText = (EditText) findViewById(R.id.edit_message);
+//        String message = editText.getText().toString();
+//        intent.putExtra(EXTRA_MESSAGE, message);
+//        startActivity(intent);
+//    }
