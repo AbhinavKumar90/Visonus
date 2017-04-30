b5e7e2c481e8ea9f5e7a23a63567c8a173dad318
AbhinavKumar90 <abhinav.kanwar@gmail.com>
2017-04-29 21:18:30 -0700

    add stuff

--- app/src/main/java/com/visonus/abhi/visonus/FirstActivity.java
+++ app/src/main/java/com/visonus/abhi/visonus/FirstActivity.java
@@ -1,23 +1,11 @@
 package com.visonus.abhi.visonus;
 
 import android.content.Intent;
-import android.net.ParseException;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-
-import java.io.BufferedReader;
-import java.io.DataOutputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
 
 public class FirstActivity extends AppCompatActivity {
 
@@ -27,7 +15,6 @@ public class FirstActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first);
-        //setContentView(R.layout.activity_first);
 
         openCameraListener();
         openGallery();
@@ -52,128 +39,87 @@ public class FirstActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("content://media/internal/images/media"));
-                startActivity(intent);
+                startActivityForResult(intent, 12345);
             }
         });
-
-
     }
 
-    //Sending image to the server using multi-part request
-    public String multipartRequest(String urlTo, String post, String filepath, String filefield) throws ParseException, IOException {
+   /* protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // if the result is capturing Image
+        super.onActivityResult(requestCode, resultCode, data);
 
-        HttpURLConnection connection = null;
-        DataOutputStream outputStream = null;
-        InputStream inputStream = null;
+        if (requestCode == 12345) {
+            if (resultCode == RESULT_OK) {
 
-        String twoHyphens = "--";
-        String boundary =  "*****"+Long.toString(System.currentTimeMillis())+"*****";
-        String lineEnd = "\r\n";
+                // successfully captured the image
+                // launching upload activity
+                launchUploadActivity(true);
 
-        String result = "";
+            } else if (resultCode == RESULT_CANCELED) {
 
-        int bytesRead, bytesAvailable, bufferSize;
-        byte[] buffer;
-        int maxBufferSize = 1*1024*1024;
+                // user cancelled Image capture
+                Toast.makeText(getApplicationContext(),
+                        "User cancelled image capture", Toast.LENGTH_SHORT)
+                        .show();
 
-        String[] q = filepath.split("/");
-        int idx = q.length - 1;
-
-        try {
-            File file = new File(filepath);
-            FileInputStream fileInputStream = new FileInputStream(file);
-
-            URL url = new URL(urlTo);
-            connection = (HttpURLConnection) url.openConnection();
-
-            connection.setDoInput(true);
-            connection.setDoOutput(true);
-            connection.setUseCaches(false);
-
-            connection.setRequestMethod("POST");
-            connection.setRequestProperty("Connection", "Keep-Alive");
-            connection.setRequestProperty("User-Agent", "Android Multipart HTTP Client 1.0");
-            connection.setRequestProperty("Content-Type", "multipart/form-data; boundary="+boundary);
-
-            outputStream = new DataOutputStream(connection.getOutputStream());
-            outputStream.writeBytes(twoHyphens + boundary + lineEnd);
-            outputStream.writeBytes("Content-Disposition: form-data; name=\"" + filefield + "\"; filename=\"" + q[idx] +"\"" + lineEnd);
-            outputStream.writeBytes("Content-Type: image/jpeg" + lineEnd);
-            outputStream.writeBytes("Content-Transfer-Encoding: binary" + lineEnd);
-            outputStream.writeBytes(lineEnd);
-
-            bytesAvailable = fileInputStream.available();
-            bufferSize = Math.min(bytesAvailable, maxBufferSize);
-            buffer = new byte[bufferSize];
-
-            bytesRead = fileInputStream.read(buffer, 0, bufferSize);
-            while(bytesRead > 0) {
-                outputStream.write(buffer, 0, bufferSize);
-                bytesAvailable = fileInputStream.available();
-                bufferSize = Math.min(bytesAvailable, maxBufferSize);
-                bytesRead = fileInputStream.read(buffer, 0, bufferSize);
+            } else {
+                // failed to capture image
+                Toast.makeText(getApplicationContext(),
+                        "Sorry! Failed to capture image", Toast.LENGTH_SHORT)
+                        .show();
             }
+        }
 
-            outputStream.writeBytes(lineEnd);
-
-            // Upload POST Data
-            String[] posts = post.split("&");
-            int max = posts.length;
-            for(int i=0; i<max;i++) {
-                outputStream.writeBytes(twoHyphens + boundary + lineEnd);
-                String[] kv = posts[i].split("=");
-                outputStream.writeBytes("Content-Disposition: form-data; name=\"" + kv[0] + "\"" + lineEnd);
-                outputStream.writeBytes("Content-Type: text/plain"+lineEnd);
-                outputStream.writeBytes(lineEnd);
-                outputStream.writeBytes(kv[1]);
-                outputStream.writeBytes(lineEnd);
-            }
-
-            outputStream.writeBytes(twoHyphens + boundary + twoHyphens + lineEnd);
-
-            inputStream = connection.getInputStream();
-            result = this.convertStreamToString(inputStream);
-
-            fileInputStream.close();
-            inputStream.close();
-            outputStream.flush();
-            outputStream.close();
-
-            return result;
-        } catch(Exception e) {
-            Log.e("MultipartRequest","Multipart Form Upload Error");
-            e.printStackTrace();
-            return "error";
+        else if(requestCode == 789 && resultCode == RESULT_OK
+                && null != data)
+        {
+            Uri selectedImage = data.getData();
+            String picturePath = getRealPathFromURI(selectedImage,
+                    this);
+            Intent i = new Intent(MainActivity.this, UploadActivity.class);
+            i.putExtra("filePath", selectedImage);
+            i.putExtra("isImage", isImage);
+            startActivity(i);
         }
     }
 
-    private String convertStreamToString(InputStream is) {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
-        StringBuilder sb = new StringBuilder();
-
-        String line = null;
-        try {
-            while ((line = reader.readLine()) != null) {
-                sb.append(line);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                is.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+    public String getRealPathFromURI(Uri contentURI, Activity context) {
+        String[] projection = { MediaStore.Images.Media.DATA };
+        @SuppressWarnings("deprecation")
+        Cursor cursor = context.managedQuery(contentURI, projection, null,
+                null, null);
+        if (cursor == null)
+            return null;
+        int column_index = cursor
+                .getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+        if (cursor.moveToFirst()) {
+            String s = cursor.getString(column_index);
+            // cursor.close();
+            return s;
         }
-        return sb.toString();
+        // cursor.close();
+        return null;
     }
+
+    private void launchUploadActivity(boolean isImage){
+        Intent i = new Intent(MainActivity.this, UploadActivity.class);
+        i.putExtra("filePath", fileUri.getPath());
+        i.putExtra("isImage", isImage);
+        startActivity(i);
+    }
+
+    private void browseImage(){
+        try {
+            Intent galleryIntent = new Intent(Intent.ACTION_PICK,
+                    android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+            // Start the Intent
+            fileUri = getOutputMediaFileUri(PICK_IMAGE);//this is m using for camera
+            Log.w("ImageAddressOnClick pr", ""+fileUri);
+            startActivityForResult(galleryIntent, 789);
+        } catch (Exception e) {
+            Toast.makeText(getApplicationContext(),
+                    e.getMessage()+"ye show hora h",
+                    Toast.LENGTH_LONG).show();
+            Log.e(e.getClass().getName(), e.getMessage(), e);
+        }*/
 }
-
-    /** Called when the user clicks the Send button */
-//    public void sendMessage(View view) {
-//        Intent intent = new Intent(this, DisplayMessageActivity.class);
-//        EditText editText = (EditText) findViewById(R.id.edit_message);
-//        String message = editText.getText().toString();
-//        intent.putExtra(EXTRA_MESSAGE, message);
-//        startActivity(intent);
-//    }
